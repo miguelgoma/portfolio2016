14a8bafcf265ea0286cea84829e0714a8dd42429
mike <utnmike@gmail.com>
2016-04-17 06:26:04 -0500

    js4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -335,7 +335,8 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="{{ asset('../js/bootstrap.min.js') }}" type="text/javascript"></script>
+<!--<script src="{{ asset('../js/bootstrap.min.js') }}" type="text/javascript"></script>-->
+<script src="http://doberman.herokuapp.com/../js/bootstrap.min.js"></script>
 <script>
     $('.carousel').carousel({
         interval: 3500
