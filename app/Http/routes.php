4fe05d7ea88219693c4b272a490194a9da740386
mike <utnmike@gmail.com>
2016-04-16 19:48:28 -0500

    d

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,3 +34,12 @@ Route::get('/test', function()
     });
 
 });
+
+Route::get('/pdf', function()
+{
+    $html = '<html><body>'
+            . '<p>Put your html here, or generate it with your favourite '
+            . 'templating system.</p>'
+            . '</body></html>';
+    return PDF::load($html, 'A4', 'portrait')->show();
+});
