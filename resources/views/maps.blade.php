23da830e5c584fd5db5107d2c6ec317223aac524
mike <utnmike@gmail.com>
2016-06-10 06:08:10 -0500

    mapsd

--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -1,8 +1,32 @@
-@extends('layouts.top-nav')
-
-@section('content')
-<img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyBpOXTHWz5Bv1hM63uP_vLTnAB2NQBiQ3g" alt="myMap">
-<img src="https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&maptype=satellite&key=AIzaSyBpOXTHWz5Bv1hM63uP_vLTnAB2NQBiQ3g" alt="myMap2">
-<img src="https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&maptype=hybrid&key=AIzaSyBpOXTHWz5Bv1hM63uP_vLTnAB2NQBiQ3g" alt="myMap3">
-<img src="https://maps.googleapis.com/maps/api/staticmap?center=Williamsburg,Brooklyn,NY&zoom=13&size=400x400&markers=color:blue%7Clabel:S%7C11211%7C11206%7C11222&key=AIzaSyBpOXTHWz5Bv1hM63uP_vLTnAB2NQBiQ3g" alt="myMap4">
-@stop
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #map {
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpOXTHWz5Bv1hM63uP_vLTnAB2NQBiQ3g&callback=initMap"
+    async defer></script>
+  </body>
+</html>
