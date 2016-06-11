e1ca48b3862140c141272a3c7efb69444b32844d
mike <utnmike@gmail.com>
2016-06-11 02:45:18 -0500

    otromaps

--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Simple Map</title>
-    <meta name="viewport" content="initial-scale=1.0">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
+    <title>Rectangles</title>
     <style>
       html, body {
         height: 100%;
@@ -11,22 +11,41 @@
         padding: 0;
       }
       #map {
-        height: 100%;
+        height: 50%;
       }
     </style>
+    <script>
+
+// This example adds a red rectangle to a map.
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {lat: 33.678, lng: -116.243},
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  });
+
+  var rectangle = new google.maps.Rectangle({
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+    map: map,
+    bounds: {
+      north: 33.685,
+      south: 33.671,
+      east: -116.234,
+      west: -116.251
+    }
+  });
+}
+
+    </script>
   </head>
   <body>
     <div id="map"></div>
-    <script>
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpOXTHWz5Bv1hM63uP_vLTnAB2NQBiQ3g&callback=initMap"
-    async defer></script>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALFYAPf_3Cc1x1HjAM20LucKuJABzQXDw&signed_in=true&callback=initMap"></script>
   </body>
 </html>
