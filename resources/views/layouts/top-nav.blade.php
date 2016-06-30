46f150104eac444c2a843e2048f8287beb83fa65
mike <utnmike@gmail.com>
2016-06-30 05:28:09 -0500

    voicesw5

--- resources/views/layouts/top-nav.blade.php
+++ resources/views/layouts/top-nav.blade.php
@@ -226,6 +226,5 @@
     <script src="{{ asset('js/app.min.js') }}"></script>
     <!-- AdminLTE for demo purposes -->
     <script src="{{ asset('js/demo.js') }}"></script>
-    @yield('includejs')
   </body>
 </html>
