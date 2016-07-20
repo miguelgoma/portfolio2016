1f7a2f5603c272bc93146f2a12f2c42840dd52c2
mike <utnmike@gmail.com>
2016-07-20 05:13:22 -0500

    graph

--- resources/views/data2.blade.php
+++ resources/views/data2.blade.php
@@ -1,1 +1,54 @@
 @extends('layouts.boxed')
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+    </script>
+
+    
+@section('main')
+<section class="content">
+          <div class="callout callout-info">
+            <h4>Tip!</h4>
+            <p>Add the layout-boxed class to the body tag to get this layout. The boxed layout is helpful when working on large screens because it prevents the site from stretching very wide.</p>
+          </div>
+          <!-- Default box -->
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Title</h3>
+              <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+                <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Start creating your amazing application!
+              <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+            </div><!-- /.box-body -->
+            <div class="box-footer">
+              Footer
+            </div><!-- /.box-footer-->
+          </div><!-- /.box -->
+        </section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+@stop
+
